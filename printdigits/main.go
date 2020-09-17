c061c64616e9d33408526665cbc7a713aa3d26d7
ADESPRES <antoine.despres@ynov.com>
2020-09-17 13:53:30 +0200

    numéro 3

---
+++ printdigits/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func main() {
+
+	i := 0
+	var aRune string = "0123456789\n"
+
+	for i <= 10 {
+
+		z01.PrintRune(rune(aRune[i]))
+		i = i - 1
+
+	}
+
+}
