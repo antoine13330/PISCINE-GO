ccf61b445f64104faa4e460bd13e08c395ac1b33
ADESPRES <antoine.despres@ynov.com>
2020-09-21 10:28:19 +0200

    go

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,12 +1,19 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	B := nb - 1
-	if nb <= 25 && nb > 0 {
+
+	if nb > 0 {
+		B := nb - 1
 		for B > 0 {
 			nb = nb * B
 			B = B - 1
 		}
+	}
+	if nb == 0 {
+		nb = nb + 1
+	}
+	if nb < 0 {
+		nb = 0
 	}
 	return nb
 }
