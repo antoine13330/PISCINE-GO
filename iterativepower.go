38bdbefa28df70e2e20ee413c4b797c645e8f351
ADESPRES <antoine.despres@ynov.com>
2020-09-21 17:06:18 +0200

    aie

--- iterativepower.go
+++ iterativepower.go
@@ -1,19 +1,15 @@
 package piscine
 
 func IterativePower(nb int, power int) int {
-	result := nb
-	if power > 1 {
-		for o := nb - 2; o <= power; o++ {
-			result = result * nb
-
-		}
-	}
-	if power == 0 {
-		result = 1
-
-	}
 	if power < 0 {
-		result = 0
+		return 0
+	} else if power == 0 {
+		return 1
+	} else {
+		result := 1
+		for i := power; i > 0; i-- {
+			result *= nb
+		}
+		return result
 	}
-	return result
 }
