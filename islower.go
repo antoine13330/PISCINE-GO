9b06b7a01134bf84194c72920ba376ebb950add0
ADESPRES <antoine.despres@ynov.com>
2020-09-23 11:24:11 +0200

    fuck

--- islower.go
+++ islower.go
@@ -6,10 +6,10 @@ func IsLower(s string) bool {
 	for i := 0; i < len(runes); i++ {
 
 		if int(runes[i]) >= 97 && int(runes[i]) <= 122 {
-			return false
+			return true
 		}
 
 	}
 
-	return true
+	return false
 }
