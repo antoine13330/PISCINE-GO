7e2f5fd06a03772d40271016008fc750fd472a0e
ADESPRES <antoine.despres@ynov.com>
2020-09-23 17:24:16 +0200

    aie

---
+++ printparams/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	arguments := os.Args
+	len := 0
+	for i := range arguments {
+		len = i
+	}
+	for i := 1; i <= len; i++ {
+		for _, k := range arguments[i] {
+			z01.PrintRune(k)
+		}
+		z01.PrintRune(10)
+	}
+
+}
