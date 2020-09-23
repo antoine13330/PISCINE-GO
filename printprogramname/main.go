e48839c86cf136465f1339785f108495ed49f41a
ADESPRES <antoine.despres@ynov.com>
2020-09-23 15:42:29 +0200

    gogogogoggogoggo

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
 
 func main() {
-	fmt.Println("printprogramname")
+	s := os.Args[0]
+	slashindex := -1
+	for i, r := range s {
+
+		if r == '/' {
+			slashindex = i
+		}
+	}
+	for _, letter := range s[slashindex+1:] {
+		z01.PrintRune(letter)
+	}
+	z01.PrintRune('\n')
 }
