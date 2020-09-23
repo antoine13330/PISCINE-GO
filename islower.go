f7ce0f7a65f511e80e30c447bc6b77057118265e
ADESPRES <antoine.despres@ynov.com>
2020-09-23 11:16:44 +0200

    aieaieaie

---
+++ islower.go
@@ -0,0 +1,15 @@
+package piscine
+
+func IsUpper(s string) bool {
+
+	runes := []rune(s)
+	for i := 0; i < len(runes); i++ {
+
+		if int(runes[i]) >= 97 && int(runes[i]) <= 122 {
+			return false
+		}
+
+	}
+
+	return true
+}
