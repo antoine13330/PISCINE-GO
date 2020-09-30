87742bb8f01e8d26581abced9a470ffa00ba065b
ADESPRES <adespres@noreply.git.ytrack.learn.ynov.com>
2020-09-30 09:15:07 +0000

    Update 'map.go'

--- map.go
+++ map.go
@@ -3,7 +3,7 @@ package piscine
 func Map(f func(int) bool, a []int) []bool {
 	var Abool = make([]bool, len(a))
 	for i, b := range a {
-      Abool[i] = f(b) 
-		return Abool
+      Abool[i] = f(b) 	
 	}
+  return Abool
 }
